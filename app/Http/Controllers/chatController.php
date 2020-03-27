7d303d8018911337c1be66a034bd0a67e18888c9
alessandrofuda <alessandro.fuda@gmail.com>
2020-03-27 15:01:32 +0100

    first commit

---
+++ app/Http/Controllers/chatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class chatController extends Controller {
+	public function index() {
+		$data['session_id'] = 'SMARTWORK_DEMO_'.time();
+		$data['flow_name'] = 'Wexplore_Smartworking_Demo';
+    	return view('smartworking_chat', $data);
+	}
+}
